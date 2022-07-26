0df6efbd625fbf5b7137264a0f0486a31dbd73d4
Watts-Blake <watts.blake52@gmail.com>
2022-07-26 14:30:31 -0700

    first proj, last question done

--- problems/04-interval-count.js
+++ problems/04-interval-count.js
@@ -1,12 +1,12 @@
 /***********************************************************************
-Write a function, `intervalCount`, that accepts a callback, a delay in 
-milliseconds, and an amount. The function should set an interval with 
+Write a function, `intervalCount`, that accepts a callback, a delay in
+milliseconds, and an amount. The function should set an interval with
 the given callback and delay, but clear the interval after the callback
 has been executed 'amount' number of times.
 
 Hint: utilize a 'closure' to your advantage
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Example
@@ -17,6 +17,14 @@ intervalCount(function() {
 ***********************************************************************/
 
 function intervalCount(cb, delay, amount) {
+  let myInt = setInterval(() => {
+    cb();
+    amount--;
+    if (amount === 0) {
+      clearInterval(myInt);
+    }
+  }, delay);
+
   // Your code here
 }
 
